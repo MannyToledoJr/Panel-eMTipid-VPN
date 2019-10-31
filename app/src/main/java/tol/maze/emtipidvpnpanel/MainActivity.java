bb4c6fada9e649667c62bec14692223c425d8cf3
MannyToledoJr <legit.mannytoledojr@gmail.com>
2019-10-31 14:31:00 +0800

    new

---
+++ app/src/main/java/tol/maze/emtipidvpnpanel/MainActivity.java
@@ -0,0 +1,31 @@
+package tol.maze.emtipidvpnpanel;
+
+import android.app.*;
+import android.os.*;
+import com.github.javiersantos.appupdater.*;
+import com.github.javiersantos.appupdater.enums.*;
+import android.support.v7.app.*;
+
+public class MainActivity extends AppCompatActivity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+		
+		//WELCOME APPUPDATER ##########
+		AppUpdater appUpdater = new AppUpdater(this);
+		appUpdater.setDisplay(Display.DIALOG);
+		appUpdater.setUpdateFrom(UpdateFrom.JSON);
+		appUpdater.setUpdateJSON("https://raw.githubusercontent.com/Mazeu31/eMTipid-VPN/master/app/updates.json");
+		appUpdater.setTitleOnUpdateAvailable("Update available");
+		appUpdater.setContentOnUpdateAvailable("Check out the latest version available of my app!");
+		appUpdater.setTitleOnUpdateNotAvailable("Update not available");
+		appUpdater.setContentOnUpdateNotAvailable("No update available. Check for updates again later!");
+		appUpdater.setButtonUpdate("Update now?");
+		appUpdater.setCancelable(false);
+		appUpdater.showAppUpdated(true);
+		appUpdater.start();
+    }
+}
